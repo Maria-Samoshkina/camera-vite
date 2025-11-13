ce8b761e50f6fee9f3485edc9d592741a4360f4b
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2025-11-13 12:25:56 +0200

    реализует рекламный слайдер для catalogPage

--- src/pages/catalog-page/promo-block.tsx
+++ src/pages/catalog-page/promo-block.tsx
@@ -1,44 +1,61 @@
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
 import { getIsPromoCamerasDataLoading, getPromoCameras } from '../../store/promo-cameras/promo-cameras-selectors';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 function PromoBlock (): JSX.Element {
 
   const promoCameras = useAppSelector(getPromoCameras);
   const isPromoCamerasLoading = useAppSelector(getIsPromoCamerasDataLoading);
 
-  const firstPromoCamera = promoCameras[0];
-
   if (isPromoCamerasLoading){
     return <div>Loading...</div>;
   }
 
-  if (!firstPromoCamera) {
+  if (!promoCameras || promoCameras.length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="banner">
-      <picture>
-        <source
-          type="image/webp"
-          srcSet={`${firstPromoCamera.previewImgWebp}, ${firstPromoCamera.previewImgWebp2x} 2x`}
-        />
-        <img
-          src={firstPromoCamera.previewImg}
-          srcSet={`${firstPromoCamera.previewImg2x} 2x`}
-          width="1280" height="280"
-          alt={firstPromoCamera.name}
-        />
-      </picture>
-      <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">{firstPromoCamera.name}</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
-        <Link
-          className="btn"
-          to={`/camera/${firstPromoCamera.id}`}
-        >Подробнее
-        </Link>
-      </p>
-    </div>
+    <Swiper
+      modules={[Pagination, Autoplay]}
+      autoplay = {{delay: 3000, disableOnInteraction: false}}
+      pagination={{ clickable: true }}
+      loop
+      className="slider"
+    >
+      {
+        promoCameras.map((promoCamera) => (
+          <SwiperSlide key = {promoCamera.id}>
+
+            <div className="banner">
+              <picture>
+                <source
+                  type="image/webp"
+                  srcSet={`${promoCamera.previewImgWebp}, ${promoCamera.previewImgWebp2x} 2x`}
+                />
+                <img
+                  src={promoCamera.previewImg}
+                  srcSet={`${promoCamera.previewImg2x} 2x`}
+                  width="1280" height="280"
+                  alt={promoCamera.name}
+                />
+              </picture>
+              <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">{promoCamera.name}</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
+                <Link
+                  className="btn"
+                  to={`/camera/${promoCamera.id}`}
+                >Подробнее
+                </Link>
+              </p>
+            </div>
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
   );
 }
 
